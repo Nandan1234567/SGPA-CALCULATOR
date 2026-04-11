f7fc0ffd08f15dd8da3b03d8c1828b15d9674aa8
Nandan1234567 <shettynandan071@gmail.com>
2026-04-11 22:19:57 +0530

    the model creation for sgpa calcuator just rough models

---
+++ Domain/Models/SgpaResult.cs
@@ -0,0 +1,36 @@
+﻿namespace SGPA_CALCULATOR.Domain.Models
+{
+    public class SgpaResult
+    {
+
+        // here we need to use the constructor to initlize or what the best prctices and to use
+
+
+        public int Id { get; set; } 
+        public string StudentName { get; set; } = string.Empty;
+
+        public string USN { get; set; } = string.Empty;
+
+        public int Semester { get; set; }   
+
+        public int Branch { get ; set; }
+
+
+        // it slected based on the subject code if subject code is cs = computer scinece 
+        // based on the branch and semester so we get the subject code related crdit points // how to do this?
+
+        // and how effective use the oop concepts so secure and  used in industry 
+        public List<SubjectResult> SubjectResults { get; set; } = new List<SubjectResult>();
+
+
+        public int TotalCredits { get; set; }   
+         
+
+        //??  what else i left
+          
+
+        // total crdit points divide by   totalCredits points = sgpa
+
+        public double SGPA { get; set; } // calculated based on grade points and credits, eg A=10*3=30, B=8*3=24, C=6*3=18, D=4*3=12, F=0*3=0
+    }
+}
---
+++ Domain/Models/SubjectResult.cs
@@ -0,0 +1,31 @@
+﻿namespace SGPA_CALCULATOR.Domain.Models
+{
+    public class SubjectResult
+    {
+
+        public int  Id { get; set; }    
+
+        public string SubjectCode { get; set; } = string.Empty; 
+
+        public string SubjectName { get; set; }  = string.Empty;
+
+        public int InternalMarks { get; set; }  
+        public int ExternalMarks { get; set; }  
+
+
+        public int TotalMarks { get; set; }
+
+        // based on total marks grade points will be added ,  where to use this ? i need to get know 
+
+        //grade ponts and crdit points here or in sgpa domain model , 
+        public int GradePoints { get; set; } // calculated based on total marks, eg A=10, B=8, C=6, D=4, F=0
+
+        public string Grade { get; set; } = string.Empty; // calculated based on total marks, eg A, B, C, D, F
+        public int Credits { get; set; } = 0;  // eg 3, 4,2 // i feel like credit is not here // as soon as subject code is assigned it  need to get credits    
+     //   public int CreditPoints { get; set; } // calculated based on grade points and credits, eg A=10*3=30, B=8*3=24, C=6*3=18, D=4*3=12, F=0*3=0
+
+
+        // where exactly use this credit points , how pdf upload calculate what subject need to do here
+
+    }
+}
