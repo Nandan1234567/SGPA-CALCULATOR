7e3e2b445a9097586b32a87e06a4d8b2d68b1638
Nandan1234567 <shettynandan071@gmail.com>
2026-04-11 11:10:00 +0530

    Add project files.

---
+++ Infrastructure/Data/SgpaDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SGPA_CALCULATOR.Infrastructure.Data
+{
+    public class SgpaDbContext : DbContext
+    {
+        public SgpaDbContext(DbContextOptions<SgpaDbContext> options) : base(options)
+        {
+        }
+
+        
+    }
+}
