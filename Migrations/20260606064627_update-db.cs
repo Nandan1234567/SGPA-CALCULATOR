392770d393724e6b325b5140e1769baf09f14b6d
Nandan1234567 <shettynandan071@gmail.com>
2026-06-06 12:21:19 +0530

    the db migration changes of ef core

---
+++ Migrations/20260606064627_update-db.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SGPA_CALCULATOR.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
