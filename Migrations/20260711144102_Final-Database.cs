672bf0f74895d18f755b5e62a1e891f611ec10f6
Nandan1234567 <shettynandan071@gmail.com>
2026-07-11 21:39:41 +0530

    clean db

---
+++ Migrations/20260711144102_Final-Database.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SGPA_CALCULATOR.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "SubjectMasters",
+                columns: new[] { "Id", "Credits", "IsNonCreditForSgpa", "Semester", "SubjectCode", "SubjectType" },
+                values: new object[] { 47, 4, false, 2, "BMATS201", "Theory" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "SubjectMasters",
+                keyColumn: "Id",
+                keyValue: 47);
+        }
+    }
+}
